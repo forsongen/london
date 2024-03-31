ce67bddc6da50a7ba2d7ef268edf059c6eee2a4b
forsongen <vienna.mccarthy@gmail.com>
2024-03-31 00:47:17 -0700

    Navbar fixes

--- nav.js
+++ nav.js
@@ -6,8 +6,4 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(navDownload => navDownload.text())
     .then(navImport => document.querySelector("#nav-import").innerHTML = navImport);
 
-    // Fetch weather and put in navbar
-    fetch('https://wttr.in/London?u&format=1')
-    .then(weatherImport => weatherImport.text())
-    .then(currentWeather => document.querySelector("#current-weather").innerHTML = currentWeather);
 });
